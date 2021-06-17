ce74d345de92483ba9a6cf65407808ddf600574d
ahanwaar <01123581321>
2021-06-17 15:17:30 +0300

    updating the stylesheet

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function changeColor() {
-  confirm('Are you sure?')
-  var favColor = prompt("what is your fav color(1.beige, 2.white, or 3.green)?")
-if(favColor == "beige"){
-  document.getElementById('main').style.backgroundColor ='beige'
-} else if(favColor == "green"){
-   document.getElementById('main').style.backgroundColor ='#8FBC8F'
-}else if(favColor == "white")
-{
-document.getElementById('main').style.backgroundColor ='white'
+    confirm('Are you sure?')
+    var favColor = prompt("what is your fav color(1.beige, 2.white, or 3.green)?")
+    if(favColor == "beige"){
+        document.getElementById('main').style.backgroundColor ='beige'
+    } else if(favColor == "green"){
+        document.getElementById('main').style.backgroundColor ='#8FBC8F'
+    }else if(favColor == "white")
+    {
+        document.getElementById('main').style.backgroundColor ='white'
 
-}else{
-  alert("Sorry the color name is incorrect!")
-}
+    }else{
+        alert("Sorry the color name is incorrect!")
+    }
 }
