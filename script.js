55456fc4ea4f0001e3db217be400eaba497426df
ahanwaar <01123581321>
2021-06-17 15:33:15 +0300

    updating js file with while loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changeColor() {
     confirm('Are you sure?')
     var favColor = prompt("what is your fav color(1.beige, 2.white, or 3.green)?")
-    while (favColor != "beige" && favColor != "white" && favColor != "green") {
+    while (favColor != "beige" && favColor != "silver" && favColor != "green") {
         favColor = prompt("you entered wrong color please try gain ")
     }
 
@@ -9,8 +9,8 @@ function changeColor() {
         document.getElementById('main').style.backgroundColor ='beige'
     } else if(favColor == "green"){
         document.getElementById('main').style.backgroundColor ='#8FBC8F'
-    }else if(favColor == "white") {
-        document.getElementById('main').style.backgroundColor = 'white'
+    }else if(favColor == "silver") {
+        document.getElementById('main').style.backgroundColor = 'silver'
     }
 }
 
