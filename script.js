d3f736b3ad7e16be803658d550937a582c4183d5
ahanwaar <01123581321>
2021-06-20 11:03:16 +0300

    updating js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+var num = prompt("how do you rate the site out of 5?")
+num = parseInt(num)
+if(num <= 5) {
+    for (var j = 0; j < num; j++) {
+        document.getElementById("rate").innerHTML += '<img src="assets/star.png" alt="star">'
+    }
+}
+
 function changeColor() {
     confirm('Are you sure?')
     var favColor = prompt("what is your fav color(1.beige, 2.silver, or 3.green)?")
-    while (favColor != "beige" && favColor != "silver" && favColor != "green") {
+    while (favColor !== "beige" && favColor !== "silver" && favColor !== "green") {
         favColor = prompt("you entered wrong color please try gain ")
     }
 
-    if(favColor == "beige"){
+    if(favColor === "beige"){
         document.getElementById('main').style.backgroundColor ='beige'
-    } else if(favColor == "green"){
+    } else if(favColor === "green"){
         document.getElementById('main').style.backgroundColor ='#8FBC8F'
-    }else if(favColor == "silver") {
+    }else if(favColor === "silver") {
         document.getElementById('main').style.backgroundColor = 'silver'
     }
 }
