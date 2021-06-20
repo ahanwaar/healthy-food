46565a06d968df3e1e619e8d1e8d300edeb8b5ac
ahanwaar <01123581321>
2021-06-20 13:19:04 +0300

    updating lab 07 requirements

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var num = prompt("how do you rate the site out of 5?")
-num = parseInt(num)
-if(num <= 5) {
-    for (var j = 0; j < num; j++) {
-        document.getElementById("rate").innerHTML += '<img src="assets/star.png" alt="star">'
+
+rate(num);
+
+function rate(num){
+    if(num <= 5) {
+        for (var j = 0; j < num; j++) {
+            document.getElementById("rate").innerHTML += '<img src="assets/star.png" alt="star">'
+        }
     }
 }
+
 
 function changeColor() {
     confirm('Are you sure?')
@@ -12,7 +17,6 @@ function changeColor() {
     while (favColor !== "beige" && favColor !== "silver" && favColor !== "green") {
         favColor = prompt("you entered wrong color please try gain ")
     }
-
     if(favColor === "beige"){
         document.getElementById('main').style.backgroundColor ='beige'
     } else if(favColor === "green"){
